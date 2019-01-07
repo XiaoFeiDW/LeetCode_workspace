fcf9a16011f24938834bfacd3bfccc9d6a54e1d0
dxh <1366356050@qq.com>
2019-01-07 18:42:12 +0800

    NumSquares_Test

--- src/dxh/queueandstack/NumSquares.java
+++ src/dxh/queueandstack/NumSquares.java
@@ -4,6 +4,12 @@ public class NumSquares {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.err.println();
+		System.err.println();
+		System.err.println();
+		System.err.println();
+		System.err.println();
+		System.err.println();
 
 	}
 
