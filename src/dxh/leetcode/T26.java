9cd4c3a0da0c4b568d43e33d6a5f9cc1654b07e6
dxh <dxh@DESKTOP-A64CSQI>
2019-06-15 13:35:40 +0800

    T26

--- src/dxh/leetcode/T26.java
+++ src/dxh/leetcode/T26.java
@@ -6,20 +6,23 @@ public class T26 {
 		// TODO Auto-generated method stub
 
 		int[] nums = {0, 1, 1, 2, 2, 3, 3, 4};
+		
 		System.err.println(removeDuplicates(nums));
 	}
 	
 	public static int removeDuplicates(int[] nums) {
 		
 		int len = 1;
-		if(nums == null) {
+		if(nums.length == 0) {
 			return 0;
 		}
-		int j = 1;
+		
+		int j = 1; //用来记录新数组中数据的索引
 		for(int i = 1; i < nums.length; i++) {
 			if(nums[i] != nums[i - 1]) {
-				nums[j++] = nums[i];
 				len++;
+				nums[j] = nums[i];
+				j++;
 			}
 		}
 		
